3d68f4af038f87fcc2e452458251d16aa5f6defe
nathan long <blazexlong@yahoo.com>
2020-03-31 21:53:43 -0700

    Added importos.my file to repositorye

---
+++ Python/importos.py
@@ -0,0 +1,6 @@
+import os
+import subprocess
+
+os.startfile('C:\\Program Files\\TeamSpeak 3 Client\\ts3client_win64.exe')
+subprocess.Popen(['C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'])
+os.system('TASKKILL /F /IM Steam.exe')
